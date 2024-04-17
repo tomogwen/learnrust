7c41bdf22a6390df697db4d00c954e70af415e41
tomogwen <tdavies@turing.ac.uk>
2024-04-17 19:12:05 +0100

    add 8 - collections

---
+++ book_examples/8_collections/src/main.rs
@@ -0,0 +1,12 @@
+// Rust's std lib includes collections: data structure containing many values
+// which live on the heap - unlike the built in array/tuple types
+
+// Vectors allow you to store a variable number of values next to each other
+// Strings allow you to store a collection of characters
+// Hash maps associate values with keys
+
+mod vectors;
+
+fn main() {
+    vectors::examples();
+}
---
+++ book_examples/8_collections/src/vectors.rs
@@ -0,0 +1,33 @@
+// separating the examples into different files
+
+fn creating_pushing_reading() {
+    let v:Vec<i32> = Vec::new();
+    let mut w = vec![1, 2, 3];  // this macro creates a Vec<i32> (as default ints are i32)
+    w.push(4);  // we can add values to a vector with push
+    println!("Vector: {:?}", w);
+
+    // we can read elements of a vector with indexing or .get()
+    let third: &i32 = &w[2];  // indexing
+    println!("Third element is {third}");
+
+    let third: Option<&i32> = v.get(2);  // getting
+    match third {
+        Some(third) => println!("Third element is {third}"),
+        None => println!("There is no third element"),
+    }
+
+}
+
+fn scope() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    // let first = &v[0]; // mutable borrow occurs here!
+    let first = v[0];  // compiles if not mutable, i.e., not borrowed/referenced
+    v.push(6);  // mutable borrow also occurs here
+    println!("{first}");
+}
+
+pub fn examples() {
+    creating_pushing_reading();
+    scope();
+}
+
