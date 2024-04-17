76d78430b37fb91c0b2451e63d93c254990e27dd
tomogwen <tdavies@turing.ac.uk>
2024-04-17 18:09:28 +0100

    finish 7.4

--- book_examples/7-4_use_as/src/lib.rs
+++ book_examples/7-4_use_as/src/lib.rs
@@ -1,8 +1,19 @@
+// we can use nested paths to bring multiple items into scope in one line, e.g.,
+use std::{cmp::Ordering, io};
+
+// or the whole module with self
+use std::io::{self, Write};
+
+// or even all modules
+use std::collections::*;
+
+
 // there are a couple of idiomatic ways to bring items with the same name into scope
 // the first is to keep the parent module
 
 use std::fmt;
 use std::io;
+
 
 fn example1() -> fmt::Result {
     // return fmt::Result
@@ -11,7 +22,6 @@ fn example1() -> fmt::Result {
 fn example2() -> io::Result<()> {
     // return io::Result
 }
-
 
 // an alternative way is with use.. as to alias the types locally
 use std::fmt::Result;
