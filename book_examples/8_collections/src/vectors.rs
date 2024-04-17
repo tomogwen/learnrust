0f527cb9469f53703e0313f0d908e705f952a443
tomogwen <tdavies@turing.ac.uk>
2024-04-17 22:55:54 +0100

    8 - collections

--- book_examples/8_collections/src/vectors.rs
+++ book_examples/8_collections/src/vectors.rs
@@ -23,11 +23,27 @@ fn borrowing() {
     // let first = &v[0]; // mutable borrow occurs here!
     let first = v[0];  // compiles if not mutable, i.e., not borrowed/referenced
     v.push(6);  // mutable borrow also occurs here
+    // even though the elements are at different ends of the vector, the borrow checker still prevents this
+    // you may e.g. have to move the whole vector on the heap when adding new elements to the end
     println!("{first}");
+}
+
+fn iterating() {
+    let mut v = vec![100, 32, 57];
+    for n_ref in &mut v {
+        // n_ref has type &i32
+        let n_plus_one: i32 = *n_ref + 1;  // dereference to get value from address of an element in v
+        println!("{n_plus_one}");
+
+        // we can also dereference n_ref to change the value
+        // as long as its `in &mut v`
+        *n_ref += 10;
+    }
+    println!("v: {:?}", v);
 }
 
 pub fn examples() {
     creating_pushing_reading();
     borrowing();
+    iterating();
 }
-
